e25b13560f98d4adbd0710cc11096b5dcf032c44
Samuel Sml <veramirandasamuel6@gmail.com>
2024-03-15 08:43:22 -0500

    database connection

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const connectDB = require("./config/db.js");
 
 const app = express();
 const port = 3000;
+
+connectDB();
 
 app.listen(port, () => {
   console.log(`Server: http://localhost:${port}`);
